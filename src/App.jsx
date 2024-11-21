24fe1120ea4229096d042ec5d583d34fe20017f3
Mary Williams <91165055+mkriscodes@users.noreply.github.com>
2024-11-21 13:41:39 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
   <div className="overflow-x-hidden text-neutral-300 antialiased seletion:bg-cyan-300 selection:text-cyan-900">
   <div className="fixed top-0 -z-10 h-full w-full">
-  <div class="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
+  <div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 transform bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]"></div>
   </div>
 
   <div className="container mx-auto px-8">
